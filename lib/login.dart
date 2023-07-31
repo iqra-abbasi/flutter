e4fe0065550ebe3929c2052c119f7ca7f8e2dc75
iqra-abbasi <139046452+iqra-abbasi@users.noreply.github.com>
2023-07-31 17:26:19 +0500

    First commit

---
+++ lib/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+     // backgroundColor: Colors.grey,
+
+      appBar: AppBar(
+        title: Text("android app"),
+        backgroundColor: Colors.pink,
+      ),
+
+
+      body:Column(
+        children: [
+
+        ],
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:app_in_java/login.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());//constructor or obj call in main
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+
+      //theme of app
+
+      themeMode:ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+
+      darkTheme: ThemeData(
+        brightness:Brightness.dark,
+        primarySwatch: Colors.pink,
+      ),
+
+    );
+  }
+}
